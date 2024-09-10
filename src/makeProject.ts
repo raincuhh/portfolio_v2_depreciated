92cff70caac3b24c2d1e55986f94630c411b7f68
raincuhh <146904648+raincuhh@users.noreply.github.com>
2024-09-10 09:26:24 +0200

    INITIAL COMMIT

---
+++ src/makeProject.ts
@@ -0,0 +1,15 @@
+enum project_langs {
+  HTML = 0,
+  CSS,
+  JS,
+  TS,
+  CPP,
+  C,
+  GDScript,
+}
+
+class project_template {
+  constructor(name: string, image_path: string, description: string) {}
+}
+
+function make_project_template(proj_templ: project_template) {}
